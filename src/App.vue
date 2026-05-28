bba7592417d2baaa9ca5f658fab689c871eb90e1
Jillian-973 <killiandumoulin02@gmail.com>
2026-05-28 15:14:49 +0200

    hero done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
+<script setup>
+import bgImage from './assets/bg.png'
+import Header from './components/header.vue'
+</script>
+
 <template>
   <div
-    class="h-screen bg-cover bg-center bg-no-repeat"
-    style="background-image: url('./assets/bg.png')"
+    class="h-screen bg-cover bg-top bg-no-repeat"
+    :style="{ backgroundImage: `url(${bgImage})` }"
   >
-    <h1>HERO SECTION</h1>
-  </div>
+    <Header />
+    <div class="flex flex-col items-center justify-center text-center gap-6 mt-50">
+      <h1 class="text-[96px] font-bold">Phronesis</h1>
+      <p class="mx-150 font-bold text-[24px]">
+        L'IA , peut s’apprendre en jouant. Phronesis réinvente la pédagogie autour de l’IA
+        générative à travers des expériences interactives, des défis et des discussions qui font
+        réfléchir.
+      </p>
+    </div>
 
-  <div>
-    <h3>test</h3>
+    <div class="flex flex-col items-center justify-center text-center gap-6 mt-50">
+      <p class="text-[36px] font-bold">Nos activités</p>
+      <img src="./assets/mouse.png" alt="Mouse Icon" class="h-20 cursor-pointer animate-bounce" />
+    </div>
   </div>
 </template>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,25 +1,21 @@
 <template>
-  <header>
-    <div>
-      <nav class="py-8">
-        <ul class="flex justify-center gap-25">
-          <li
-            class="h-8 px-14 rounded-xl border-2 border-orange-400 bg-orange-100/70 font-bold text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(251,146,60,0.6)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
-          >
-            <a href="/">Home</a>
-          </li>
-          <li
-            class="h-8 px-14 rounded-xl border-2 border-pink-400 bg-pink-100/70 font-bold text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(239,60,173,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
-          >
-            <a href="/about">About</a>
-          </li>
-          <li
-            class="h-8 px-14 rounded-xl border-2 border-fuchsia-400 bg-fuchsia-100/70 text-black font-bold text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(217,70,239,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
-          >
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <nav class="py-8">
+    <ul class="flex justify-center gap-25">
+      <li
+        class="h-8 px-14 rounded-xl border-2 border-orange-400 bg-orange-100/70 font-bold text-black text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(251,146,60,0.6)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
+      >
+        <a href="/">Accueil</a>
+      </li>
+      <li
+        class="h-8 px-14 rounded-xl border-2 border-pink-400 bg-pink-100/70 font-bold text-black text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(239,60,173,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
+      >
+        <a href="/about">Activités</a>
+      </li>
+      <li
+        class="h-8 px-14 rounded-xl border-2 border-fuchsia-400 bg-fuchsia-100/70 text-black font-bold text-1xl leading-none flex items-center justify-center shadow-[0_0_12px_rgba(217,70,239,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
+      >
+        <a href="/contact">A propos</a>
+      </li>
+    </ul>
+  </nav>
 </template>
