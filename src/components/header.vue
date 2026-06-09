05e1d9591b2538db98c166eb2e6236ca26963c2e
Jillian-973 <killiandumoulin02@gmail.com>
2026-06-09 17:06:57 +0200

    commit

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,12 +4,12 @@
       <li
         class="h-8 px-6 sm:px-10 md:px-14 rounded-xl border-2 border-orange-400 bg-orange-100/70 font-bold text-black text-sm sm:text-base leading-none flex items-center justify-center shadow-[0_0_12px_rgba(251,146,60,0.6)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
       >
-        <a href="index.html">Accueil</a>
+        <a href="/main">Accueil</a>
       </li>
       <li
         class="h-8 px-6 sm:px-10 md:px-14 rounded-xl border-2 border-pink-400 bg-pink-100/70 font-bold text-black text-sm sm:text-base leading-none flex items-center justify-center shadow-[0_0_12px_rgba(239,60,173,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
       >
-        <a href="activite.html">Activités</a>
+        <a href="/activite">Activités</a>
       </li>
       <li
         class="h-8 px-6 sm:px-10 md:px-14 rounded-xl border-2 border-fuchsia-400 bg-fuchsia-100/70 text-black font-bold text-sm sm:text-base leading-none flex items-center justify-center shadow-[0_0_12px_rgba(217,70,239,0.5)] backdrop-blur-sm transition-all duration-300 hover:scale-105"
