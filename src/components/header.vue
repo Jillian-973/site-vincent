258e1030cd41b4f4aab56e4f63cb06e5658694ff
Jillian-973 <killiandumoulin02@gmail.com>
2026-07-02 03:18:09 +0200

    ficing route accueil

--- src/components/header.vue
+++ src/components/header.vue
@@ -8,7 +8,7 @@
       <li
         class="h-8 px-3 sm:px-10 md:px-14 rounded-xl border-2 border-orange-400 bg-orange-100/70 font-bold text-black text-xs sm:text-base leading-none flex items-center justify-center backdrop-blur-sm transition-all duration-300 hover:scale-105 shadow-[0_0_18px_4px_rgba(251,146,60,0.7)] hover:shadow-[0_0_30px_8px_rgba(251,146,60,0.95)]"
       >
-        <a href="/accueil">Accueil</a>
+        <a href="/">Accueil</a>
       </li>
       <li
         class="h-8 px-3 sm:px-10 md:px-14 rounded-xl border-2 border-pink-400 bg-pink-100/70 font-bold text-black text-xs sm:text-base leading-none flex items-center justify-center backdrop-blur-sm transition-all duration-300 hover:scale-105 shadow-[0_0_18px_4px_rgba(239,60,173,0.7)] hover:shadow-[0_0_30px_8px_rgba(239,60,173,0.95)]"
